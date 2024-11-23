c5ff4abca31bca6b368e43b8b960982b0211e7c4
Ragas30 <130697528+Ragas30@users.noreply.github.com>
2024-11-24 00:00:48 +0700

    add aos to app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+import { useEffect } from "react";
 import Footer from "./component/Footer";
 import Navbar from "./component/Navbar";
 import Wellcome from "./component/Wellcome";
 import ProgramingLang from "./ProgramingLang";
+import Aos from "aos";
 
 function App() {
+  useEffect(() => {
+    Aos.init({
+      once: false,
+      duration: 1000,
+    });
+  });
   return (
     <>
       <Navbar />
       <Wellcome />
       <ProgramingLang />
 
-      <Footer/>
+      <Footer />
     </>
   );
 }

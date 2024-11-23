ccbd68a7a8d4f2e9b7d1158fe07c9e0e8031b274
Ragas30 <130697528+Ragas30@users.noreply.github.com>
2024-11-23 23:44:34 +0700

    renew create mapping and renew logo

--- src/ProgramingLang.jsx
+++ src/ProgramingLang.jsx
@@ -1,18 +1,17 @@
 import ProgramingList from "./component/ProgramingList";
 import Title from "./component/Title";
+import { data } from "./data/data";
 
 export default function ProgramingLang() {
   return (
     <>
       <div className="mt-10 z-50 mb-10">
-        <Title Title={"Programing Language"} />
+        <Title title={"Programing Language"} />
       </div>
-      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 ">
-        <ProgramingList image="/icon/html5.png" language="HTML" deskripsi="HTML merupakan bahasa pemrograman yang digunakan untuk membuat halaman web yang dinamis dan interaktif." />
-        <ProgramingList image="/icon/css3.png" language="CSS" deskripsi="CSS merupakan bahasa pemrograman yang digunakan untuk membuat halaman web yang dinamis dan interaktif." />
-        <ProgramingList image="/icon/javascript.png" language="Javascript" deskripsi="Javascript merupakan bahasa pemrograman yang digunakan untuk membuat halaman web yang dinamis dan interaktif." />
-        <ProgramingList image="/icon/react.png" language="React" deskripsi="React merupakan framework yang digunakan untuk membuat halaman web yang dinamis dan interaktif." />
-        <ProgramingList image="/icon/tailwind.png" language="Tailwind" deskripsi="Tailwind merupakan framework yang digunakan untuk membuat halaman web yang dinamis dan interaktif." />
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 items-center">
+        {data.map((item, index) => {
+          return <ProgramingList key={index} image={item.Image} language={item.Language} deskripsi={item.Deskripsi} />;
+        })}
       </section>
     </>
   );
---
+++ src/data/data.js
@@ -0,0 +1,62 @@
+export const data = [
+    {
+        Image: "/icon/html5.png",
+        Language: "HTML",
+        Deskripsi: "HTML merupakan bahasa pemrograman yang digunakan untuk membuat halaman web yang dinamis dan interaktif.",
+    },
+    {
+        Image: "/icon/css3.png",
+        Language: "CSS",
+        Deskripsi: "CSS merupakan bahasa pemrograman yang digunakan untuk membuat halaman web yang dinamis dan interaktif.",
+    },
+    {
+        Image: "/icon/javascript.png",
+        Language: "Javascript",
+        Deskripsi: "Javascript merupakan bahasa pemrograman yang digunakan untuk membuat halaman web yang dinamis dan interaktif.",
+    },
+    {
+        Image: "/icon/php.png",
+        Language: "CSS",
+        Deskripsi: "PHP merupakan bahasa pemrograman yang digunakan untuk membuat halaman web yang dinamis dan interaktif.",
+    },
+    {
+        Image: "/icon/laravel.png",
+        Language: "Tailwind",
+        Deskripsi: "Laravel merupakan framework yang digunakan untuk membuat halaman web yang dinamis dan interaktif.",
+    },
+    {
+        Image: "/icon/react.png",
+        Language: "React",
+        Deskripsi: "React merupakan framework yang digunakan untuk membuat halaman web yang dinamis dan interaktif.",
+    },
+    {
+        Image: "/icon/c.png",
+        Language: "C Language",
+        Deskripsi: "C merupakan bahasa pemrograman yang digunakan untuk membuat halaman web yang dinamis dan interaktif.",
+    },
+    {
+        Image: "/icon/c++.png",
+        Language: "C++ Language",
+        Deskripsi: "C++ merupakan bahasa pemrograman yang digunakan untuk membuat halaman web yang dinamis dan interaktif.",
+    },
+    {
+        Image: "/icon/tailwind.png",
+        Language: "Tailwind",
+        Deskripsi: "Tailwind merupakan framework yang digunakan untuk membuat halaman web yang dinamis dan interaktif.",
+    },
+    {
+        Image: "/icon/github.png",
+        Language: "github",
+        Deskripsi: "Github Merupakan Platform Yang digunakan untuk menyimpan segala macam dokumentasi codingan saya",
+    },
+    {
+        Image: "/icon/boostrap5.png",
+        Language: "Boostrap 5",
+        Deskripsi: "Github Merupakan Platform Yang digunakan untuk menyimpan segala macam dokumentasi codingan saya",
+    },
+    {
+        Image: "/icon/mysql.png",
+        Language: "Database MySql",
+        Deskripsi: "MySql Merupakan tempat penggunaan integrasi penyimpanan Database",
+    },
+]
